c4b3b117aba862e619249b906693d2658c2cda1a
Federico Robledo <federicorobledo85@gmail.com>
2022-11-18 00:18:07 -0300

    agrega estilos responsive

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,31 +1,43 @@
 import "../styles/navbar.css";
 import { NavLink } from "react-router-dom";
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
 
 const Navbar = ({ size, setShow }) => {
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive_nav");
+  };
   return (
     <>
-      <nav>
-        <div className="nav_box">
-          <span className="my_shop" onClick={() => setShow(true)}>
-            Guitar Shop
-          </span>
+      <header>
+        <h1>Guitar Shop</h1>
+        <nav ref={navRef}>
           <div className="cart" onClick={() => setShow(false)}>
             <span>
               <i className="fas fa-cart-plus"></i>
             </span>
             <span>{size}</span>
-            <NavLink to="/login">
-              <button className="botones">login</button>
-            </NavLink>
-            <NavLink to="/products">
-              <button className="botones">Productos</button>
-            </NavLink>
-            <NavLink to="/">
-              <button className="botones">home</button>
-            </NavLink>
           </div>
-        </div>
-      </nav>
+
+          <NavLink to="/login">
+            <button className="botones">Login</button>
+          </NavLink>
+          <NavLink to="/products">
+            <button className="botones">Comprar</button>
+          </NavLink>
+          <NavLink to="/">
+            <button className="botones">Home</button>
+          </NavLink>
+          <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+            <FaTimes />
+          </button>
+        </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+          <FaBars />
+        </button>
+      </header>
     </>
   );
 };
--- src/Components/Slideshow.jsx
+++ src/Components/Slideshow.jsx
@@ -6,7 +6,7 @@ import { NavLink } from "react-router-dom";
 const Slideshow = () => {
   return (
     <>
-      <div className="container1">
+      <div className="cards1">
         <div className="row">
           <div className="col">
             <h4 className="title" color="black">
@@ -17,32 +17,27 @@ const Slideshow = () => {
             </NavLink>
           </div>
         </div>
-        <div className="row">
-          <div className="col">
-            <div className="cards1">
-              {list.map((values) => {
-                // filtra por propiedad rating del producto y lo verifica con if
-                const { id, title, price, image, rating } = values;
-                if (rating >= 5) {
-                  return (
-                    <>
-                      <div className="card" key={id}>
-                        <div className="card-header">
-                          <img src={image} alt={title} />
-                        </div>
-                        <div className="card-body">
-                          <h2 className="titleProduct">{title}</h2>
-                          <span className="price">${price}</span>
-                        </div>
-                      </div>
-                    </>
-                  );
-                }
-              })}
-            </div>
-          </div>
-        </div>
+        {list.map((values) => {
+          // filtra por propiedad rating del producto y lo verifica con if
+          const { id, title, price, image, rating } = values;
+          if (rating >= 5) {
+            return (
+              <>
+                <div className="card" key={id}>
+                  <div className="card-header">
+                    <img src={image} alt={title} />
+                  </div>
+                  <div className="card-body">
+                    <h2 className="titleProduct">{title}</h2>
+                    <span className="price">${price}</span>
+                  </div>
+                </div>
+              </>
+            );
+          }
+        })}
       </div>
+      y
     </>
   );
 };
